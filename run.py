d486b2c7a72fbc76da0e3154253ae7f5d55ba9f7
zimmoc <128923890+zimmoc@users.noreply.github.com>
2023-12-26 22:22:34 +0100

    Added api and function to test api

--- run.py
+++ run.py
@@ -1,6 +1,25 @@
 from pprint import pprint
 from todoist_api_python.api import TodoistAPI
 
+api = TodoistAPI("755a8992983b0540febf3a6c66c4d9c16a7b9d31")
 
+def get_todays_tasks():
+    """
+    Get tasks market with the "Today" tag from todoist
+    """
+    try:
+        tasks = api.get_tasks(
+            filter='today'
+        )
+    except Exception as error:
+        print(error)
+    
+    return tasks
+
+
+
+def main():
+    tasks = get_todays_tasks()
+    pprint(tasks)
 
 main()
