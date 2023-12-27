9c81e6c21d3ac8cd89de971e7c7efb9567f6d221
zimmoc <128923890+zimmoc@users.noreply.github.com>
2023-12-27 01:35:58 +0100

    Created function to complete tasks

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from pprint import pprint
 from todoist_api_python.api import TodoistAPI
 import sys
 
-api = TodoistAPI("755a8992983b0540febf3a6c66c4d9c16a7b9d31")
+api_key = "755a8992983b0540febf3a6c66c4d9c16a7b9d31"
+api = TodoistAPI(api_key)
 
 
 ### MENUS
@@ -11,33 +12,31 @@ def display_menu():
     """
     Menu to navigate program functions
     """
-    print("\n")
-    print("Options:")
+    print("\nOptions:")
     print("1. Show tasks")
     print("2. Create tasks")
-    print("3. Change account")
+    print("3. Complete task")
     print("4. Exit program")
-    print("\n")
 
 def user_choice(user_choice):
     """
     Sub-menus depending on user choice from menu
     """
     if user_choice == 1:
-        print("\n")
-        print("Which tasks do you wanna view?")
+        print("\nWhich tasks do you want to view?")
         print("1. Overdue")
         print("2. Today")
         print("3. Weekly")
         print("4. Return to previous menu")
-        print("\n")
     elif user_choice == 2:
         print("Create task")
         create_task()
         return_to_menu()
         
     elif user_choice == 3:
-        print("You selected Option 3.")
+        complete_task()
+        return_to_menu()
+
     elif user_choice == 4:
         print("Exiting program...")
         sys.exit()
@@ -47,9 +46,8 @@ def return_to_menu():
     Restarts main() function to give the impression that the user
     is sent back to main menu
     """
-    print("\n")
-    input("Press Enter to go back to menu...")
-    print("\n")
+    print("\nPress Enter to go back to the menu...")
+    input("\n")
     main()
 
 def return_to_tasks_menu():
@@ -57,9 +55,8 @@ def return_to_tasks_menu():
     Restarts main() function to give the impression that the user
     is sent back to main menu
     """
-    print("\n")
-    input("Press Enter to return to tasks menu...")
-    print("\n")
+    print("\nPress Enter to return to the tasks menu...")
+    input("\n")
     user_input = 1
     user_choice(user_input)
     user_input = get_user_input()
@@ -110,16 +107,20 @@ def get_todays_tasks():
         tasks = api.get_tasks(
             filter='today'
         )
+
+        if not tasks:
+            print("\nAll tasks completed for today.")
+            return
+
     except Exception as error:
         print(error)
     
-    print("\n")
-    print("Tasks due today")
+    print("\nTasks due today:")
     print("----------")
-    for task in tasks:
+    for id, task in enumerate(tasks, start=1):
         task_name = task.content
         due_date = task.due.date
-        print(f"{task_name} - {due_date}")
+        print(f"{id}. {task_name} - {due_date}")
     print("----------")
 
 def get_overdue_tasks():
@@ -130,15 +131,19 @@ def get_overdue_tasks():
         tasks = api.get_tasks(
             filter='overdue'
         )
+
+        if not tasks:
+            print("\nNo overdue tasks.")
+            return
+
     except Exception as error:
         print(error)
-    print("\n")
-    print("Overdue tasks")
+    print("\nOverdue tasks:")
     print("----------")
-    for task in tasks:
+    for id, task in enumerate(tasks, start=1):
         task_name = task.content
         due_date = task.due.date
-        print(f"{task_name} - {due_date}")
+        print(f"{id}. {task_name} - {due_date}")
     print("----------")
 
 def get_weekly_tasks():
@@ -149,24 +154,35 @@ def get_weekly_tasks():
         tasks = api.get_tasks(
             filter='due before: mon'
         )
+        
+        if not tasks:
+            print("\nAll tasks completed for the week.")
+            return
+
     except Exception as error:
         print(error)
     
-    print("\n")
-    print("Tasks due this week")
+    print("\nTasks due this week:")
     print("----------")
-    for task in tasks:
+    for id, task in enumerate(tasks, start=1):
         task_name = task.content
         due_date = task.due.date
-        print(f"{task_name} - {due_date}")
+        task_id = task.id
+        print(f"{id}. {task_name} - {due_date}")
     print("----------")
+
+    """
+    Returning tasks list so the complete_task function can call it
+    """
+    return tasks
+
 
 ## Create tasks
     
 def create_task():
-    print("\n")
+    print("\nCreate Task:")
     task_name = input("Enter task: ")
-    task_due = input("Enter task due date(ex. tomorrow, 2025-01-01): ")
+    task_due = input("Enter task due date (e.g., tomorrow, 2025-01-01): ")
     try:
         task = api.add_task(
             content=str(task_name),
@@ -178,11 +194,38 @@ def create_task():
     
     created_name = task.content
     created_due_date = task.due.date
-    print("\n")
-    print("Successfully created task")
+    print("\nSuccessfully created task:")
     print("----------")
     print(f"Task name: {created_name}")
     print(f"With due date: {created_due_date}")
+
+## Complete task
+    
+def complete_task():
+    try:
+        tasks = get_weekly_tasks()
+        
+        if not tasks:
+            return
+        
+        selected_task_number = int(input("\nEnter the number of the task you want to complete: "))
+        
+        if 1 <= selected_task_number <= len(tasks):
+            selected_task = tasks[selected_task_number - 1]
+            task_name = selected_task.content
+            task_date = selected_task.due.date
+            task_id = selected_task.id
+            is_success = api.close_task(task_id=task_id)
+            print(f"\nSuccessfully completed task:")
+            print("----------")
+            print(f"{task_name} - {task_date}")
+            print("----------")
+        else:
+            print("Invalid task number. Please enter a valid number.")
+    except Exception as error:
+        print(error)
+
+
 
 ### MAIN
     
